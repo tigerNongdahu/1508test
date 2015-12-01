cf0a272a0d65ebef8eb59e9b15d7af4ce035206e
农大浒 <tarena42@tiger.local>
2015-12-01 10:13:08 +0800

    第二次提交

---
+++ tigerTestGit/tigerTestGit/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  tigerTestGit
+//
+//  Created by tarena42 on 15/12/1.
+//  Copyright © 2015年 tarena42. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
